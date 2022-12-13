e0af8fae0b68ee3d13b990a6b4c2dab92fea1b5f
Max Salvatore <mmsalva@umich.edu>
2022-12-13 10:59:45 -0500

    fix incorrrect namespace

--- fn/quick_mgi_partial_correlation.R
+++ fn/quick_mgi_partial_correlation.R
@@ -1,19 +1,24 @@
+library(data.table)
+library(ppcor)
+
 quick_mgi_partial_correlation <- function(x) {
   cca <- complete.cases(sub_pim[, .SD, .SDcols = c(x[1], x[2])])
 
   if (sum(cca) == dim(sub_pim)[1]) {
-    a <- ppcor::pcor.test(sub_pim[[x[1]]],
-                          sub_pim[[x[2]]],
-                          x1_mgi,
-                          method = "pearson")
+    a <- pcor.test(sub_pim[[x[1]]],
+                   sub_pim[[x[2]]],
+                   x1_mgi,
+                   method = "pearson")
   } else if (sum(cca) < dim(sub_pim)[1] && sum(cca != 0)) {
-    a <- ppcor::pcor.test(sub_pim[cca, .SD, .SDcols = x[1]],
-                          sub_pim[cca, .SD, .SDcols = x[2]],
-                          x2_mgi[cca, ],
-                          method = "pearson")
+    a <- pcor.test(sub_pim[cca, .SD, .SDcols = x[1]],
+                   sub_pim[cca, .SD, .SDcols = x[2]],
+                   x2_mgi[cca, ],
+                   method = "pearson")
   }
+  
   cbind(
-    as.data.table::data.table(to = x[1], from = x[2]),
-    data.table::as.data.table(a)
+    data.table(to = x[1], from = x[2]),
+    as.data.table(a)
   )
+  
 }
