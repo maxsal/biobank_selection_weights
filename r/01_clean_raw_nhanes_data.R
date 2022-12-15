a3676d4616a32e22e8c87bb1fc46625f38eef46b
Max Salvatore <mmsalva@umich.edu>
2022-12-15 15:30:26 -0500

    add medical conditions to nhanes data

--- r/01_clean_raw_nhanes_data.R
+++ r/01_clean_raw_nhanes_data.R
@@ -3,13 +3,14 @@
 #           '/data/public/nhanes'
 # outputs:  cleaned nhanes data
 # author:   max salvatore
-# date:     20221207
+# date:     20221215
 
 # libraries, functions, and options --------------------------------------------
 library(data.table)
 library(haven)
 library(glue)
 library(progress)
+library(cli)
 
 source("fn/cleaning-utils.R")
 
@@ -20,6 +21,7 @@ output_path        <- "data/public/nhanes/"
 save               <- TRUE
 
 # demographics -----------------------------------------------------------------
+cli_alert_info("processing demographics...")
 demo <- read_xpt(glue("{nhanes_data_path}{nhanes_data_prefix}_DEMO.XPT")) |>
   as.data.table()
 demo_vars <- c("SEQN", "RIAGENDR", "RIDAGEYR", "RIDRETH1", "RIDRETH3",
@@ -55,6 +57,7 @@ demo[, race_eth3 := fcase(
   race_eth3 == "7", "Other Race - Including Multi-Racial",
   default = "Unknown"
 )]
+demo[, nhw := as.numeric(race_eth1 == "Non-Hispanic White")]
 demo[, education := fcase(
   education == "1", "Less than 9th grade",
   education == "2", "9-11th grade (includes 12th grade with no diploma)",
@@ -72,6 +75,7 @@ demo[, marital_status := fcase(
 demo[, female := as.numeric(gender == "Female")]
 
 # alcohol ----------------------------------------------------------------------
+cli_alert_info("processing alcohol...")
 alc <- read_xpt(glue("{nhanes_data_path}{nhanes_data_prefix}_ALQ.XPT")) |>
   as.data.table()
 alc_vars <- c("SEQN", "ALQ111", "ALQ121")
@@ -98,6 +102,7 @@ alc[, drinker := fcase(
 )]
 
 # smoking ----------------------------------------------------------------------
+cli_alert_info("processing smoking...")
 smk <- read_xpt(glue("{nhanes_data_path}{nhanes_data_prefix}_SMQ.XPT")) |>
   as.data.table()
 smk_vars <- c("SEQN", "SMQ020", "SMQ040")
@@ -123,20 +128,82 @@ smk[, smoker := fcase(
   default = "Unknown"
 )]
 
+# medical conditions -----------------------------------------------------------
+cli_alert_info("processing medical conditions...")
+mcq <- read_xpt(glue("{nhanes_data_path}{nhanes_data_prefix}_MCQ.XPT")) |>
+  as.data.table()
+mcq_vars <- c("SEQN", "MCQ010", "MCQ080", "MCQ160E", "MCQ160F", "MCQ220",
+              "MCQ230A")
+mcq <- mcq[, ..mcq_vars]
+setnames(mcq,
+         mcq_vars,
+         c("id", "asthma_ever", "overweight_ever", "heart_attack", "stroke",
+           "cancer_ever", "cancer_first"))
+yes_no_recode <- function(x) {
+  fcase(
+    x == 1, "Yes",
+    x == 2, "No",
+    default = "Unknown"
+  )
+}
+mcq[, c("asthma_ever", "overweight_ever", "heart_attack", "stroke",
+        "cancer_ever") := lapply(.SD, yes_no_recode),
+    .SDcols = c("asthma_ever", "overweight_ever", "heart_attack", "stroke",
+                "cancer_ever")]
+mcq[, id := as.character(id)]
+mcq[, cancer_first := fcase(
+  cancer_first == 10, "Bladder",
+  cancer_first == 11, "Blood",
+  cancer_first == 12, "Bone",
+  cancer_first == 13, "Brain",
+  cancer_first == 14, "Breast",
+  cancer_first == 15, "Cervix (cervical)",
+  cancer_first == 16, "Colon",
+  cancer_first == 17, "Esophagus (esophageal)",
+  cancer_first == 18, "Gallbladder",
+  cancer_first == 19, "Kidney",
+  cancer_first == 20, "Larynx/ windpipe",
+  cancer_first == 21, "Leukemia",
+  cancer_first == 22, "Liver",
+  cancer_first == 23, "Lung",
+  cancer_first == 24, "Lymphoma/ Hodgkin's disease",
+  cancer_first == 25, "Melanoma",
+  cancer_first == 26, "Mouth/tongue/lip",
+  cancer_first == 27, "Nervous system",
+  cancer_first == 28, "Ovary (ovarian)",
+  cancer_first == 29, "Pancreas (pancreatic)",
+  cancer_first == 30, "Prostate",
+  cancer_first == 31, "Rectum (recal)",
+  cancer_first == 32, "Skin (non-melanoma)",
+  cancer_first == 33, "Skin (don't know what kind)",
+  cancer_first == 34, "Soft tissue (muscle or fat)",
+  cancer_first == 35, "Stomach",
+  cancer_first == 36, "Testis (testicular)",
+  cancer_first == 37, "Thyroid",
+  cancer_first == 38, "Uterus (uterine)",
+  cancer_first %in% c(39, 66, 77, 99), "Other",
+  default = "None reported"
+)]
+
+
 # merge ------------------------------------------------------------------------
+cli_alert_info("merging...")
 out <- Reduce(\(x, y) {
     merge.data.table(x = x, y = y, by = "id", all.x = TRUE)
   },
-  list(demo, alc, smk))
+  list(demo, alc, smk, mcq))
 replace_missing(data = out,
                 cols = c("race_eth1", "race_eth3", "education",
                          "marital_status", "drink_ever", "drink_last_year",
                          "drinker", "smoke_100", "smoke_now", "smoker"),
                 new_value = "Unknown")
-
 if (save == TRUE) {
   fwrite(out,
          file = glue("{output_path}{nhanes_data_prefix}",
                      "_NHANES_CLEAN.txt"),
          sep  = "\t")
 }
+
+cli_alert_success(glue(
+  "nhanes data cleaned and saved: {output_path}{nhanes_data_prefix}",
+  "_NHANES_CLEAN.txt"))
