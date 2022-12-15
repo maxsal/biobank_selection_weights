36d59fcc7f744c0b2c3e00904496bf537e5f212e
Max Salvatore <mmsalva@umich.edu>
2022-12-15 10:59:05 -0500

    initial commit of pca-phers with optparsify

--- r/03_calculate_pca_phers.R
+++ r/03_calculate_pca_phers.R
@@ -16,6 +16,7 @@ suppressPackageStartupMessages({
   library(SPAtest)
   library(ggrepel)
   library(cli)
+  library(optparse)
 })
 
 set.seed(61787)
@@ -29,17 +30,43 @@ mytheme <- gridExtra::ttheme_default(
   colhead = list(fg_params = list(cex = 0.5)),
   rowhead = list(fg_params = list(cex = 0.5)))
 
+# optparse list ----------------------------------------------------------------
+option_list <- list(
+  make_option("--outcome", type = "character", default = "",
+              help = "Outcome phecode"),
+  make_option("--mgi_version", type = "character", default = "20210318",
+              help = "Version of MGI data [default = 20210318]"),
+  make_option("--ukb_version", type = "character", default = "20221117",
+              help = "Version of UKB data [default = 20221117]"),
+  make_option("--time_threshold", type = "numeric", default = "0",
+              help = glue("Time threshold for the phenome data ",
+                          "[default = 0]")),
+  make_option("--pc_var_explain", type = "numeric", default = "0.95",
+              help = glue("Cumulative variation explained by PCs threshold ",
+                          "[default = 0.95]"))
+)
+
+parser <- OptionParser(usage="%prog [options]", option_list = option_list)
+
+args <- parse_args(parser, positional_arguments = 0)
+opt <- args$options
+print(opt)
+
 # specifications ---------------------------------------------------------------
-mgi_version           <- "20210318"       # mgi phenome version
-ukb_version           <- "20221117"       # ukb phenome version
-outcome               <- "157"
-pc_var_explain        <- 0.95
-time_threshold        <- 1
-derivation_cohort     <- "mgi"
+mgi_version       <- opt$mgi_version
+ukb_verion        <- opt$ukb_version
+outcome           <- opt$outcome
+pc_var_explain    <- opt$pc_var_explain
+time_threshold    <- opt$time_threshold
+derivation_cohort <- "mgi"
 
 mgi_data_path <- glue("data/private/mgi/{mgi_version}/",
                       "X{gsub('X', '', outcome)}")
-mgi_results_path <- glue("results/mgi/{mgi_version}/X{gsub('X', '', outcome)}")
+mgi_results_path <- glue("results/mgi/{mgi_version}/X{gsub('X', '', outcome)}/",
+                         "pca")
+if (!dir.exists(mgi_results_path)) {
+  dir.create(mgi_results_path, recursive = TRUE)
+}
 
 ## pull file paths corresponding to the data version specified
 file_paths <- get_files(mgi_version = mgi_version, ukb_version = ukb_version)
@@ -176,7 +203,9 @@ p <- results_short |>
                  "{pheinfo[phecode == gsub('X', '', outcome), description]}"),
     caption = glue("Cumulative variation explained by PCs: {pc_var_explain}; ",
                    "n PCs = {format(length(pcs_mod[1, ]), big.mark = ',')}; ",
-                   "p-value threshold = 0.05/{length(pcs_mod[1, ])}")
+                   "t = {t_threshold}; ",
+                   "derivation cohort: {derivation_cohort}; ",
+                   "application cohort: mgi")
   ) +
   theme_minimal() +
   theme(
@@ -189,7 +218,9 @@ p <- results_short |>
     )
 
 ggsave(
-  filename = "test_pca_manhattan.pdf",
+  filename = glue("{mgi_results_path}/mgi_X{gsub('X', '', outcome)}_",
+                  "t{time_threshold}_pve{pc_var_explain}_",
+                  "pc_mahanttan.pdf"),
   plot     = p,
   width = 8, height = 6,
   device = cairo_pdf)
@@ -212,27 +243,35 @@ p2 <- results |>
     y = betas_m2sig,
     col = color
   ), shape = 16) +
-  labs(
-    x = "Phenotypes",
-    y = "Beta",
-    title = glue("Associations with phecode {outcome}: ",
-                 "{pheinfo[phecode == gsub('X', '', outcome), description]}")
-  ) +
-  theme_minimal() +
-  theme(
-    legend.position  = "",
-    legend.text      = element_text(size = 10),
-    legend.title     = element_blank(),
-    axis.text.x      = element_text(angle = 60, hjust = 1, vjust = 1),
-    text = element_text(size = 12)) +
   scale_colour_manual(values = unique(as.character(results[, color]))) +
   geom_label_repel(aes(x = c(1:nrow(results)), y = betas_m2sig, label = desc),
                    label.size = 0.1, force = 2, size = 2, 
                    label.padding = 0.1, point.padding = unit(0.2, "lines"),
-                   segment.alpha  = 0.3)
+                   segment.alpha  = 0.3) +
+  labs(
+    x = "Phenotypes",
+    y = "Beta",
+    title = glue("Associations with phecode {outcome}: ",
+                 "{pheinfo[phecode == gsub('X', '', outcome), description]}"),
+    caption = glue("Cumulative variation explained by PCs: {pc_var_explain}; ",
+                   "n PCs = {format(length(pcs_mod[1, ]), big.mark = ',')}; ",
+                   "t = {t_threshold}; ",
+                   "derivation cohort: {derivation_cohort}; ",
+                   "application cohort: mgi")
+  ) +
+  theme_minimal() +
+  theme(
+    plot.caption     = element_text(hjust = 0),
+    legend.position  = "",
+    legend.text      = element_text(size = 10),
+    legend.title     = element_blank(),
+    axis.text.x      = element_text(angle = 60, hjust = 1, vjust = 1),
+    text = element_text(size = 12))
 
 ggsave(
-  filename = "test_pca_betas.pdf",
+  filename = glue("{mgi_results_path}/mgi_X{gsub('X', '', outcome)}_",
+                  "t{time_threshold}_pve{pc_var_explain}_",
+                  "pc_betas.pdf"),
   plot     = p2,
   width = 8, height = 6,
   device = cairo_pdf)
@@ -242,6 +281,4 @@ cli_alert_info("calculating phers...")
 mgi_phers <- as.matrix(exclude_mgi_pim) %*% as.matrix(results[, betas_m2sig])
 mgi_phers <- (mgi_phers - mean(mgi_phers)) / sd(mgi_phers)
 
-cli_h1("PCA-PheRS for {outcome} a success!")
-cli_alert_success("Manhattan and Beta plots saved: ")
-cli_alert_success("PheRS saved: ")
+cli_alert_success("PCA-PheRS for {outcome} a success!")
