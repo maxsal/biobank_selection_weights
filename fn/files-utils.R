55b04459d93c5d10db17934ce9441177f3551b91
Max Salvatore <mmsalva@umich.edu>
2023-02-09 13:41:50 -0500

    add another helper file to files-utils

--- fn/files-utils.R
+++ fn/files-utils.R
@@ -11,15 +11,16 @@ get_files <- function(mgi_version = "20220822", ukb_version = "20221117") {
     mgi_smk_file   <- "/net/junglebook/magic_data/ehr_data_20210318/HPI5635_SocialHx_DEIDENTIFIED.txt"
     mgi_bmi_file   <- "/net/junglebook/magic_data/ehr_data_20210318/HPI5635_Anthropometrics_DEIDENTIFIED.txt"
   } else {
-    mgi_path             <- glue("/net/junglebook/magic_data/EHRdata/{mgi_version}/")
-    mgi_icd9_file        <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_ICD9_Phecodes_Birthyears.Rsav")
-    mgi_icd10_file       <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_ICD10_Phecodes_Birthyears.Rsav")
-    mgi_phecode_dsb_file <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_Phecodes_Birthyears.Rsav")
-    mgi_pim0_file        <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_20220802_PEDMASTER_0.txt")
-    mgi_cov_file         <- glue("{mgi_path}MGI_{mgi_version}.txt")
-    mgi_demo_file        <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/Demographics_{as.Date(mgi_version, '%Y%m%d')}.txt")
-    mgi_smk_file         <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/SocialHx_{as.Date(mgi_version, '%Y%m%d')}.txt")
-    mgi_bmi_file         <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/Anthropometrics_{as.Date(mgi_version, '%Y%m%d')}.txt")
+    mgi_path              <- glue("/net/junglebook/magic_data/EHRdata/{mgi_version}/")
+    mgi_icd9_file         <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_ICD9_Phecodes_Birthyears.Rsav")
+    mgi_icd10_file        <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_ICD10_Phecodes_Birthyears.Rsav")
+    mgi_phecode_dsb_file  <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_Phecodes_Birthyears.Rsav")
+    mgi_pim0_file         <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_PEDMASTER_0.txt")
+    mgi_cov_file          <- glue("{mgi_path}MGI_{mgi_version}.txt")
+    mgi_phe_overview_file <- glue("{mgi_path}phenomes/UNFILTERED_{mgi_version}/UNFILTERED_{mgi_version}_Phenotype_Overview_All_Phecodes1plus.Rsav")
+    mgi_demo_file         <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/Demographics_{as.Date(mgi_version, '%Y%m%d')}.txt")
+    mgi_smk_file          <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/SocialHx_{as.Date(mgi_version, '%Y%m%d')}.txt")
+    mgi_bmi_file          <- glue("/net/junglebook/magic_data/Data_Pulls_from_Data_Office/{mgi_version}/Anthropometrics_{as.Date(mgi_version, '%Y%m%d')}.txt")
   }
   
   if (ukb_version == "20221117") {
@@ -38,8 +39,9 @@ get_files <- function(mgi_version = "20220822", ukb_version = "20221117") {
   )
   
   if (mgi_version != "20210318") {
-    mgi_out[["phecode_dsb_file"]] <- mgi_phecode_dsb_file
-    mgi_out[["cov_file"]]         <- mgi_cov_file
+    mgi_out[["phecode_dsb_file"]]  <- mgi_phecode_dsb_file
+    mgi_out[["cov_file"]]          <- mgi_cov_file
+    mgi_out[["phe_overview_file"]] <- mgi_phe_overview_file
   }
   
   return(list(
