f2d6ab8b5a4bec79a07121d001954c72c86ea82d
Max Salvatore <mmsalva@umich.edu>
2023-02-27 14:59:51 -0500

    update superlearner

--- r/02_super_learner.R
+++ r/02_super_learner.R
@@ -22,6 +22,11 @@ suppressPackageStartupMessages({
   library(rsample)      # data splitting
   library(SuperLearner)
   library(xgboost)
+  library(ranger)
+  library(glmnet)
+  library(e1071)
+  library(bartMachine)
+  library(prettyunits)
 })
 
 # optparse list ---
@@ -41,29 +46,20 @@ option_list <- list(
   make_option("--strata", type = "character", default = "case",
               help = glue("Strata (outcome variable) for distributing between train/test sets ",
                           "[default = %default]")),
-  make_option("--n_trees", type = "numeric", default = "500",
-              help = glue("Number of trees to grow in random forest ",
-                          "[default = %default]")),
-  make_option("--n_vip", type = "numeric", default = "20",
-              help = glue("Number of variables to include in variable importance plot ",
-                          "[default = %default]")),
   make_option("--seed", type = "numeric", default = "1234",
               help = glue("Set seed for reproducibility ",
                           "[default = %default]")),
-  make_option("--n_core_prop", type = "numeric", default = "0.5",
+  make_option("--n_core_prop", type = "numeric", default = "0.75",
               help = glue("Proportion of available cores to use ",
                           "[default = %default]")),
   make_option("--discovery_cohort", type = "character", default = "mgi",
               help = glue("Cohort to use as discovery cohort (mgi / ukb) ",
                           "[default = %default]")),
-  make_option("--n_mtry", type = "numeric", default = 6,
-              help = glue("Number of values of mtry to consider ",
+  make_option("--folds", type = "numeric", default = 5,
+              help = glue("Number of folds in SuperLearner cross validation ",
                           "[default = %default]")),
-  make_option("--n_node_size", type = "numeric", default = 5,
-              help = glue("Number of values of node_size to consider ",
-                          "[default = %default]")),
-  make_option("--n_samp_frac", type = "numeric", default = 4,
-              help = glue("Number of values of sample to consider ",
+  make_option("--inner_folds", type = "numeric", default = NULL,
+              help = glue("Number of folds in SuperLearner external cross validation ",
                           "[default = %default]"))
 )
 parser <- OptionParser(usage = "%prog [options]", option_list = option_list)
@@ -80,11 +76,14 @@ if (parallel::detectCores() == 1) {
   n_cores <- parallel::detectCores() * opt$n_core_prop
 }
 
+options(mc.cores = n_cores)
+external_cohort <- ifelse(opt$discovery_cohort == "mgi", "ukb", "mgi")
+
 source("fn/expandPhecodes.R")
 source("fn/files-utils.R")
 
 # check output folder exists ---------------------------------------------------
-out_path <- glue("results/{coh}/{coh_version}/X{outc}/random_forest/",
+out_path <- glue("results/{coh}/{coh_version}/X{outc}/super_learner/",
      coh = opt$discovery_cohort,
      coh_version = ifelse(opt$discovery_cohort == "mgi", opt$mgi_version, ifelse(opt$discovery_cohort == "ukb", opt$ukb_version, NA)),
      outc = gsub("X", "", opt$outcome))
@@ -153,102 +152,154 @@ if (opt$discovery_cohort == "mgi") {
 
 # below is function or script
 cli_alert("splitting data...")
-if (!is.null(opt$strata)) {
-  data_split <- initial_split(data,
-                              prop   = opt$split_prop,
-                              strata = opt$strata)
+
+train_obs <- sample(seq_len(nrow(data)), size = round( nrow(data) *  opt$split_prop ))
+
+data_train <- data[train_obs, ]
+data_test  <- data[!train_obs, ]
+
+train_ids    <- data_train[, .(id, case)]
+test_ids     <- data_test[, .(id, case)]
+external_ids <- external[, .(id, case)]
+
+data_train_covs <- data_train[, !c("case")]
+data_train_y    <- data_train[, case]
+
+data_test_covs <- data_test[, !c("case")]
+data_test_y    <- data_test[, case]
+
+external_covs <- external[, !c("id", "case")]
+external_y    <- external[, case]
+
+# fitting SuperLearner ---------------------------------------------------------
+SL_library <- list("SL.ranger", "SL.glm", c("SL.glmnet", "screen.corP"),
+                   "SL.xgboost", "SL.svm")
+cli_alert("Using the following libraries for SuperLearner:")
+print(SL_library)
+
+if (!is.null(opt$inner_folds)) {
+  cli_alert("Running SuperLearner - set opt$inner_folds to execute external cross validation")
+  super_learner <- SuperLearner(
+    Y          = data_train_y,
+    X          = data_train_covs,
+    SL.library = SL_library,
+    family     = binomial(),
+    cvControl  = list(V = opt$folds)
+  )
+  super_learner
+  cli_alert_info("SuperLearner took {prettyunits::pretty_sec(super_learner$times$everything['elapsed'])} to run")
+  sl <- super_learner
 } else {
-  data_split <- initial_split(data,
-                              prop = opt$split_prop)
-}
-
-data_train <- training(data_split)
-data_test  <- testing(data_split)
-
-test_ids <- data_test[, .(id, case)]
-
-data[, id := NULL]
-
-# full grid search
-hyper_grid <- expand.grid(
-  mtry        = round(seq(5, round(ncol(data_train) / 2), length.out = opt$n_mtry)),
-  node_size   = round(seq(3, round(nrow(data_train) / 100), length.out = opt$n_node_size)),
-  sample_frac = seq(0.5, 0.8, length.out = opt$n_samp_frac),
-  oob_rmse    = 0
-) |> as.data.table()
-
-cli_alert("Trying {opt$n_mtry} values of mtry, {opt$n_node_size} of node size, {opt$n_samp_frac} values of sample fraction ({nrow(hyper_grid)} combinations)")
-
-cli_progress_bar(total = nrow(hyper_grid), "Optimizing hyperparameters")
-for (i in 1:nrow(hyper_grid)) {
-  model <- ranger(
-    formula         = case ~ .,
-    data            = data_train,
-    num.trees       = opt$n_trees,
-    mtry            = hyper_grid$mtry[i],
-    min.node.size   = hyper_grid$node_size[i],
-    sample.fraction = hyper_grid$sample_frac[i],
-    num.threads     = n_cores
+  cv_super_learner <- CV.SuperLearner(
+    Y              = data_train_y,
+    X              = data_train_covs,
+    SL.library     = SL_library,
+    family         = binomial(),
+    parallel       = "multicore",
+    cvControl      = list(V = opt$folds),
+    innerCvControl = list(list(V = opt$inner_folds))
   )
-  hyper_grid$oob_rmse[i] <- sqrt(model$prediction.error)
-  cli_progress_update()
+  summary(cv_super_learner)
+  cv_super_learner$whichDiscreteSL
+  table(simplify2array(cv_super_learner$whichDiscreteSL))
+  cv_super_learner_plot <- plot(cv_super_learner) +
+    cowplot::theme_minimal_grid()
+  ggsave(plot = cv_super_learner_plot,
+         filename = glue("{out_path}mgid_ukbe_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_csl_methods_plot.pdf"),
+         width = 6, height = 6, device = cairo_pdf)
+  
+  print(review_weights(cv_super_learner), digits = 3)
+  sl <- cv_super_learner
 }
 
-hyper_grid[order(oob_rmse),][1, ]
 
-oob_rmse <- vector(mode = "numeric", length = 100)
-
-cli_progress_bar(total = length(oob_rmse), "run using optimal settings...")
-for (i in seq_along(oob_rmse)) {
-  optimal_ranger <- ranger(
-    formula         = case ~ .,
-    data            = data_train,
-    num.trees       = opt$n_trees,
-    mtry            = hyper_grid[order(oob_rmse), ][1, mtry],
-    min.node.size   = hyper_grid[order(oob_rmse), ][1, node_size],
-    sample.fraction = hyper_grid[order(oob_rmse), ][1, sample_frac],
-    importance      = "impurity",
-    num.threads     = n_cores
-  )
-  oob_rmse[i] <- sqrt(optimal_ranger$prediction.error)
-  cli_progress_update()
-}
 
 # predicting -------------------------------------------------------------------
 suppressMessages({
-  pred_opt     <- predict(optimal_ranger, data_test, type = "response")
-  pred_opt_roc <- pROC::roc(data_test[, case], pred_opt[["predictions"]])
-  pred_opt_auc <- pROC::ci.auc(data_test[, case], pred_opt[["predictions"]])
+  pred_test     <- predict(sl, data_test_covs, onlySL = TRUE)
+  pred_test_roc <- pROC::roc(data_test_y, pred_test[["pred"]][, 1])
+  pred_test_auc <- pROC::ci.auc(data_test_y, pred_test[["pred"]][, 1])
   
-  pred_oth     <- predict(optimal_ranger, external, type = "response")
-  pred_oth_roc <- pROC::roc(external[, case], pred_oth[["predictions"]])
-  pred_oth_auc <- pROC::ci.auc(external[, case], pred_oth[["predictions"]])
+  pred_ext     <- predict(sl, external_covs, onlySL = TRUE)
+  pred_ext_roc <- pROC::roc(external_y, pred_ext[["pred"]][, 1])
+  pred_ext_auc <- pROC::ci.auc(external_y, pred_ext[["pred"]][, 1])
 })
 
 pretty_print <- function(x, r = 3) {
   paste0( round(x[2], r), " (", round(x[1], r), ", ", round(x[3], r), ")")
 }
 
+# outputs ----------------------------------------------------------------------
+
+## SuperLearner object
+saveRDS(sl, file = glue("{out_path}{opt$discovery_cohort}d_{ifelse({opt$discovery_cohort} == 'mgi', 'ukb', 'mgi')}e_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_super_learner.rds"))
+
+## dataset containin ID, case status, phers_raw, and phers (mean-standardized)
+phers_from_pred <- function(case_data, predicted) {
+  out <- cbind(case_data, pred = predicted)
+  out[, phers := ((pred - mean(pred, na.rm = TRUE)) / sd(pred, na.rm = TRUE))][]
+}
+test_phers     <- phers_from_pred(case_data = test_ids, pred = pred_test[["pred"]][, 1])[, `:=` (cohort = opt$discovery_cohort, split = "test")]
+external_phers <- phers_from_pred(case_data = external_ids, pred = pred_ext[["pred"]][, 1])[, `:=` (cohort = external_cohort, split = "external")]
+
+write_fst(
+  x = test_phers,
+  path = glue("{out_path}{opt$discovery_cohort}d_{opt$discovery_cohort}e_t{opt$time_threshold}_test_phers.fst")
+)
+write_fst(
+  x = external_phers,
+  path = glue("{out_path}{opt$discovery_cohort}d_{external_cohort}e_t{opt$time_threshold}_external_phers.fst")
+)
+
+## auc and or summary table
 in_stuff <- data.table(
-  sensitivity = pred_opt_roc$sensitivities,
-  specificity = pred_opt_roc$specificities,
+  sensitivity = pred_test_roc$sensitivities,
+  specificity = pred_test_roc$specificities,
   test_data   = "Hold-out test data"
 )
 out_stuff <- data.table(
-  sensitivity = pred_oth_roc$sensitivities,
-  specificity = pred_oth_roc$specificities,
+  sensitivity = pred_ext_roc$sensitivities,
+  specificity = pred_ext_roc$specificities,
   test_data   = "External data"
 )
 
 auc_sum <- data.table(
   test_data = c("Hold-out test data", "External data"),
   cohort    = c(opt$discovery_cohort, ifelse(opt$discovery_cohort == "mgi", "ukb", "mgi")),
-  auc_est   = c(pred_opt_auc[2], pred_oth_auc[2]),
-  auc_lo    = c(pred_opt_auc[1], pred_oth_auc[1]),
-  auc_hi    = c(pred_opt_auc[3], pred_oth_auc[3]),
-  auc_print = c(pretty_print(pred_opt_auc), pretty_print(pred_oth_auc))
+  auc_est   = c(pred_test_auc[2], pred_ext_auc[2]),
+  auc_lo    = c(pred_test_auc[1], pred_ext_auc[1]),
+  auc_hi    = c(pred_test_auc[3], pred_ext_auc[3]),
+  auc_print = c(pretty_print(pred_test_auc), pretty_print(pred_ext_auc))
 )
 
+extractr <- function(x) {
+  mod <- glm(case ~ phers, data = x, family = binomial())
+  suppressMessages(y <- confint(mod))
+  data.table(
+    or_est = exp(coef(mod)[["phers"]]),
+    or_lo = exp(y["phers", 1]),
+    or_hi = exp(y["phers", 2])
+  )[, print := paste0(round(or_est, 3), " (", round(or_lo, 3), ", ", round(or_hi, 3), ")")][]
+}
+
+or_sum <- rbindlist(list(
+  extractr(test_phers)[, cohort := opt$discovery_cohort],
+  extractr(external_phers)[, cohort := external_cohort]
+))
+
+total_sum <- merge.data.table(
+  auc_sum,
+  or_sum,
+  by = "cohort"
+)
+
+total_sum
+fwrite(
+  x   = total_sum,
+  file = glue("{out_path}{opt$discovery_cohort}d_{external_cohort}e_t{opt$time_threshold}_summary.txt")
+)
+
+## auc plots --
 auc_plot <- rbindlist(list(in_stuff, out_stuff))  |>
   ggplot(aes(x = 1 - specificity, y = sensitivity, color = test_data)) +
   geom_abline(lty = 3) +
@@ -263,7 +314,7 @@ auc_plot <- rbindlist(list(in_stuff, out_stuff))  |>
   labs(
     title    = glue("AUC for X{gsub('X', '', opt$outcome)} at t{opt$time_threshold}"),
     subtitle = glue("discovery = {opt$discovery_cohort}, external = {ifelse(opt$discovery_cohort == 'mgi', 'ukb', 'mgi')}"),
-    caption  = str_wrap(glue("N_trees = {opt$n_trees}, mtry = {hyper_grid[order(oob_rmse), ][1, mtry]}, node size = {hyper_grid[order(oob_rmse), ][1, node_size]}, sample fraction = {hyper_grid[order(oob_rmse), ][1, sample_frac]}"), width = 100)
+    caption  = str_wrap(glue("CV folds = {opt$folds}, train/test prop = {opt$split_prop}"), width = 100)
   ) +
   coord_equal() +
   cowplot::theme_minimal_grid() +
@@ -276,112 +327,58 @@ ggsave(plot = auc_plot,
        filename = glue("{out_path}mgid_ukbe_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_rf_auc.pdf"),
        width = 6, height = 6, device = cairo_pdf)
 
-## VIP plot
-vip_data <- data.table(
-  variable = names(optimal_ranger$variable.importance),
-  importance = optimal_ranger$variable.importance
-)[order(-importance), ][1:opt$n_vip, ] |>
-  merge.data.table(p[, .(phecode, description, group, color)], by.x = "variable", by.y = "phecode", all.x = TRUE)
-vip_data[, full_name := ifelse(is.na(description), variable, paste0(variable, ": ", description))]
-
-cols <- unique(vip_data[, .(group, color)])
-colors <- cols[, color]
-names(colors) <- cols[, group]
-
-vip_plot <- vip_data |>
-  ggplot(aes(x = reorder(full_name, importance), y = importance, fill = group)) +
-  geom_bar(stat = "identity") +
+# phers distribution by case status ---
+test_phers_dist_plot <- test_phers |>
+  ggplot(aes(x = phers)) +
+  geom_density(aes(fill = factor(case), color = factor(case)), alpha = 0.5) +
+  scale_fill_OkabeIto() +
+  scale_color_OkabeIto() +
   labs(
-    title    = glue("VIP for X{gsub('X', '', opt$outcome)} at t{opt$time_threshold}"),
-    subtitle = glue("discovery = {opt$discovery_cohort}, external = {ifelse(opt$discovery_cohort == 'mgi', 'ukb', 'mgi')}"),
-    caption  = str_wrap(glue("N_trees = {opt$n_trees}, mtry = {hyper_grid[order(oob_rmse), ][1, mtry]}, node size = {hyper_grid[order(oob_rmse), ][1, node_size]}, sample fraction = {hyper_grid[order(oob_rmse), ][1, sample_frac]}"), width = 100),
-    x = "",
-    y = "Importance"
+    title = glue("X{gsub('X', '', opt$outcome)} PheRS distribution by case status at t{opt$time_threshold}"),
+    subtitle = glue("SuperLearner model in {toupper(opt$discovery_cohort)} hold out test sample"),
+    x = "PheRS (mean standardized)",
+    y = "Density",
+    caption = str_wrap(glue("Discovery cohort = {opt$discovery_cohort}; CV folds = {opt$folds}, train/test prop = {opt$split_prop}"), width = 100)
   ) +
-  scale_fill_manual(values = colors) +
-  scale_x_discrete(labels = \(x) stringr::str_wrap(x, width = 50)) +
-  guides(fill = guide_legend(ncol = 3, byrow = TRUE)) +
-  coord_flip() +
   cowplot::theme_minimal_grid() +
   theme(
-    plot.caption    = element_text(hjust = 0),
-    legend.position = "bottom",
-    legend.title    = element_blank()
+    legend.position = "top",
+    legend.title = element_blank(),
+    plot.caption = element_text(hjust = 0)
   )
-ggsave(plot = vip_plot,
-       filename = glue("{out_path}mgid_ukbe_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_vip.pdf"),
-       width = 10, height = 8, device = cairo_pdf)
-  
-saveRDS(optimal_ranger, file = glue("{out_path}{opt$discovery_cohort}d_{ifelse({opt$discovery_cohort} == 'mgi', 'ukb', 'mgi')}e_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_optimal_rf.rds"))
+ggsave(plot = test_phers_dist_plot,
+       filename = glue("{out_path}mgid_ukbe_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_test_phers_dist.pdf"),
+       width = 6, height = 6, device = cairo_pdf)
 
+external_phers_dist_plot <- external_phers |>
+  ggplot(aes(x = phers)) +
+  geom_density(aes(fill = factor(case), color = factor(case)), alpha = 0.5) +
+  scale_fill_OkabeIto() +
+  scale_color_OkabeIto() +
+  labs(
+    title = glue("X{gsub('X', '', opt$outcome)} PheRS distribution by case status at t{opt$time_threshold}"),
+    subtitle = glue("SuperLearner model in {external_cohort} external sample"),
+    x = "PheRS (mean standardized)",
+    y = "Density",
+    caption = str_wrap(glue("Discovery cohort = {opt$discovery_cohort}; CV folds = {opt$folds}, train/test prop = {opt$split_prop}"), width = 100)
+  ) +
+  cowplot::theme_minimal_grid() +
+  theme(
+    legend.position = "top",
+    legend.title = element_blank(),
+    plot.caption = element_text(hjust = 0)
+  )
+ggsave(plot = external_phers_dist_plot,
+       filename = glue("{out_path}mgid_ukbe_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_external_phers_dist.pdf"),
+       width = 6, height = 6, device = cairo_pdf)
 
-if (opt$discovery_cohort == "mgi") {
-  mgi <- data.table(
-    phers  = ((pred_opt[["predictions"]] - mean(pred_opt[["predictions"]], na.rm = TRUE)) / sd(pred_opt[["predictions"]], na.rm = TRUE)),
-    cohort = "mgi",
-    type = "test set")
-  mgi <- cbind(mgi, test_ids)
-} else {
-  mgi <- data.table(
-    phers  = ((pred_oth[["predictions"]] - mean(pred_oth[["predictions"]], na.rm = TRUE)) / sd(pred_oth[["predictions"]], na.rm = TRUE)),
-    cohort = "mgi",
-    type = "external")
-  mgi <- cbind(mgi, d_ids)
-}
-
-if (opt$discovery_cohort == "ukb") {
-  ukb <- data.table(
-    phers  = ((pred_opt[["predictions"]] - mean(pred_opt[["predictions"]], na.rm = TRUE)) / sd(pred_opt[["predictions"]], na.rm = TRUE)),
-    cohort = "ukb",
-    type = "test set")
-  ukb <- cbind(ukb, test_ids)
-} else {
-  ukb <- data.table(
-    phers  = ((pred_oth[["predictions"]] - mean(pred_oth[["predictions"]], na.rm = TRUE)) / sd(pred_oth[["predictions"]], na.rm = TRUE)),
-    cohort = "ukb",
-    type = "external")
-  ukb <- cbind(ukb, u_ids)
-}
-
-mgi_mod <- glm(case ~ phers, data = mgi, family = binomial())
-ukb_mod <- glm(case ~ phers, data = ukb, family = binomial())
-extractr <- function(x) {
-  suppressMessages(y <- confint(x))
-  data.table(
-    or_est = exp(coef(x)[["phers"]]),
-    or_lo = exp(y["phers", 1]),
-    or_hi = exp(y["phers", 2])
-  )[, print := paste0(round(or_est, 3), " (", round(or_lo, 3), ", ", round(or_hi, 3), ")")][]
-}
-or_sum <- rbindlist(list(
-  cbind(data.table(cohort = "mgi"), extractr(mgi_mod)),
-  cbind(data.table(cohort = "ukb"), extractr(ukb_mod))
-))
-
-total_sum <- merge.data.table(
-  auc_sum,
-  or_sum,
-  by = "cohort"
-)
-
-total_sum
-
+## summary list object
 list(
   "discovery_cohort"      = opt$discovery_cohort,
-  "external_cohort"       = ifelse(opt$discovery_cohort == "mgi", "ukb", "mgi"),
-  "discovery_test_phers"  =  ifelse(opt$discovery_cohort == "mgi", mgi, ukb),
-  "external_phers"        = ifelse(opt$discovery_cohort == "mgi", ukb, mgi),
+  "external_cohort"       = external_cohort,
   "total_summary"         = total_sum,
   "discovery_sense_spec"  = in_stuff,
   "external_sense_spec"   = out_stuff,
-  "vip_table"             = data.table(
-    variable = names(optimal_ranger$variable.importance),
-    importance = optimal_ranger$variable.importance
-  )[order(-importance), ],
-  "optimal_forest_object" = optimal_ranger,
-  "hyper_grid"            = hyper_grid,
-  "auc_plot"              = auc_plot,
-  "vip_plot"              = vip_plot,
   "out_path"              = out_path,
   "optparse_list"         = opt
 ) |> saveRDS(file = glue("{out_path}{opt$discovery_cohort}d_{ifelse({opt$discovery_cohort} == 'mgi', 'ukb', 'mgi')}e_X{gsub('X', '', opt$outcome)}_t{opt$time_threshold}_summary.rds"))
