f4834443edaa667f937e7bd9dfb175cbbc241fc1
Max Salvatore <mmsalva@umich.edu>
2023-04-02 17:35:49 -0400

    update summarizer

--- fn/summarize-utils.r
+++ fn/summarize-utils.r
@@ -7,7 +7,7 @@ summarizer_func <- function(x, var_name = NULL) {
     out <- data.table(
       val = as.character(c(0, 1)),
       N   = c(sum(x == 0, na.rm = TRUE), sum(x == 1, na.rm = TRUE)),
-      nas = length(is.na(x))
+      nas = sum(is.na(x))
     )[,
       prop := round(N * 100 / length(na.omit(x)), 1)
       ][, `:=` (
@@ -22,7 +22,8 @@ summarizer_func <- function(x, var_name = NULL) {
       sd     = sd(x, na.rm = TRUE),
       median = median(x, na.rm = TRUE),
       N      = length(na.omit(x)),
-      nas    = length(is.na(x))
+      val    = "Continuous",
+      nas    = sum(is.na(x))
     )[, `:=` (
       print = paste0(
         format(round(mean, 2), big.mark = ",", nsmall = 2),
@@ -36,7 +37,7 @@ summarizer_func <- function(x, var_name = NULL) {
     out <- data.table(
       val = levels(x),
       N   = map_dbl(levels(x), \(level) sum(x == level, na.rm = TRUE)),
-      nas = length(is.na(x))
+      nas = sum(is.na(x))
     )[,
       prop := round(N * 100 / length(na.omit(x)), 1)
       ][, `:=` (
@@ -48,7 +49,7 @@ summarizer_func <- function(x, var_name = NULL) {
     out <- data.table(
       val = unique(x),
       N   = map_dbl(unique(x), \(level) sum(x == level, na.rm = TRUE)),
-      nas = length(is.na(x))
+      nas = sum(is.na(x))
     )[,
       prop := round(N * 100 / length(na.omit(x)), 1)
     ][, `:=` (
@@ -62,18 +63,82 @@ summarizer_func <- function(x, var_name = NULL) {
   if (!is.null(var_name)) {
     out[, variable := var_name]
   }
+  first_cols <- c("variable", "val", "print", "print_unit")
+  setcolorder(out, c(first_cols, setdiff(names(out), first_cols)))
   return(out)
 }
 
 summarizer <- function(x, col_names = NULL, as_tibble = FALSE) {
   if (is.null(col_names)) {
-    out <- rbindlist(mapply(summarizer_func, x, names(x)), use.names = TRUE, fill = TRUE)
+    out <- rbindlist(list(
+        data.table(variable = "N", val = "Count", print = format(x[, .N], big.mark = ",")),
+        rbindlist(mapply(summarizer_func, x, names(x)), use.names = TRUE, fill = TRUE)
+    ), use.names = TRUE, fill = TRUE)
   } else {
-    out <- rbindlist(mapply(summarizer_func, x[, ..col_names], col_names), use.names = TRUE, fill = TRUE)
+    out <- rbindlist(list(
+        data.table(variable = "N", val = "Count", print = format(x[, .N], big.mark = ",")),        
+        rbindlist(mapply(summarizer_func, x[, ..col_names], col_names), use.names = TRUE, fill = TRUE)
+    ), use.names = TRUE, fill = TRUE)
   }
   if (as_tibble) {
     tibble::as_tibble(out)
   } else {
     out
   }
+}
+
+# summarize EHR data
+phecode_dsb_summarizer <- function(x) {
+
+    first_phe <- x[ x[, .I[which.min(dsb)], id][, V1] ]
+    last_phe  <- x[ x[, .I[which.max(dsb)], id][, V1] ]
+    ehr_cov   <- merge.data.table(
+        first_phe[, .(id, first = dsb)],
+        last_phe[, .(id, last = dsb)],
+        by = "id"
+    )[,
+    ehr_days := as.numeric(last - first)
+    ][,
+    ehr_years := round(ehr_days/365.25, 1)]
+
+    rbindlist(list(
+    # number of people
+    data.table(variable = "Number of people",
+               print    = format(unique(x[, .(id)])[, .N], big.mark = ","),
+               val      = "count"),
+
+    # encounters per person
+    v_sum(unique(cond[, .(id, dsb)])[, .N, id][, N],
+          var_name = "Encounters per person, unique"),
+          
+    # total phecodes per person
+    v_sum(summary(cond[, .N, id][, N]),
+          var_name = "Phecodes per person, total"),
+
+    # unique phecodes per person
+    v_sum(unique(cond[, .(id, phecode)])[, .N, id][, N],
+          var_name = "Phecodes per person, unique")
+    ,
+
+    # length follow-up in days
+    v_sum(ehr_cov[, ehr_days],
+          var_name = "Length of EHR follow-up, days"),
+    
+    # length of follow-up in years
+    v_sum(ehr_cov[, ehr_years],
+          var_name = "Length of EHR follow-up, years", other_round = 1)
+    ), use.names = TRUE, fill = TRUE)
+}
+
+v_sum <- function(x, var_name, mean_round = 1, other_round = 0) {
+    data.table(
+        variable = var_name,
+        val      = c("min", "median", "mean", "max"),
+        print    = c(
+            format(round(min(x, na.rm = TRUE), other_round), big.mark = ",", nsmall = other_round),
+            format(round(median(x, na.rm = TRUE), other_round), big.mark = ",", nsmall = other_round),
+            format(round(mean(x, na.rm = TRUE), mean_round), big.mark = ",", nsmall = mean_round),
+            format(round(max(x, na.rm = TRUE), other_round), big.mark = ",", nsmall = other_round)
+            )
+    )
 }
